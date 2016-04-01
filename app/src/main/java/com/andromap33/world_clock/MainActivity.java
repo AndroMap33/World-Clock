737506fdf58bb4ce33a56f92910b394bdba71c50
anujgupta61 <anujgupta2004@gmail.com>
2016-04-01 21:26:19 +0530

    building basic UI

--- app/src/main/java/com/andromap33/world_clock/MainActivity.java
+++ app/src/main/java/com/andromap33/world_clock/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner countrySpinner = (Spinner) findViewById(R.id.spinner) ;
+        ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(this , R.array.country_name , android.R.layout.simple_spinner_item) ;
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) ;
+        countrySpinner.setAdapter(countryAdapter) ;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
