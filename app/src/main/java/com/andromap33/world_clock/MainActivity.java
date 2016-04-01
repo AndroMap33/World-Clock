a425733e820d7f6e0ae8796813f7ad8bc3205746
mohit83k <mohit83k@gmail.com>
2016-04-02 05:20:37 +0530

    Modifying the spinner and country name

--- app/src/main/java/com/andromap33/world_clock/MainActivity.java
+++ app/src/main/java/com/andromap33/world_clock/MainActivity.java
@@ -1,18 +1,19 @@
 package com.andromap33.world_clock;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String countryname = "India" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,20 +21,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Spinner countrySpinner = (Spinner) findViewById(R.id.spinner) ;
+        final Spinner countrySpinner = (Spinner) findViewById(R.id.spinner) ;
         ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(this , R.array.country_name , android.R.layout.simple_spinner_item) ;
         countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) ;
         countrySpinner.setAdapter(countryAdapter) ;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            TextView countryName = (TextView) findViewById(R.id.country_name) ;
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                countryname = countrySpinner.getSelectedItem().toString() ;
+                countryName.setText(countryname) ;
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                countryName.setText(countryname) ;
+            }
+
         });
+
+        /* System.out.println("What the hell") ;
+        TextView countryName = (TextView) findViewById(R.id.country_name) ;
+        countryname = countrySpinner.getSelectedItem().toString() ;
+        countryName.setText(countryname) ;
+        */
     }
+
+    /* public void show(View view) {
+        Spinner countrySpinner = (Spinner) findViewById(R.id.spinner) ;
+        TextView countryName = (TextView) findViewById(R.id.country_name) ;
+        countryName.setText(countrySpinner.getSelectedItem().toString()) ;
+    } */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,4 +75,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
